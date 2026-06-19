35c412666e367789cce056319c0001645a5ba308
kali-red-tylbf <yana.sutton@ace.tamut.edu>
2026-06-18 20:03:59 -0500

    Updated New Code - Assignment Complete

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const imgTag = document.querySelector("img");
 const formTag = document.querySelector("form");
+const outputTag = document.querySelector("output");
 formTag.onsubmit = handleSubmit;
 
 const apiKey = sessionStorage.getItem("apiKey");
-debugger;
+const isInvalid = apiKey === null;
+if (isInvalid) {
+  console.log("There is no valid API key.");
+} else {
+  displayImage(apiKey);
+}
 async function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
   const apiKey = form.elements.apiKey.value;
+  displayImage(apiKey);
+}
+
+async function displayImage(apiKey) {
   const data = {
     "api-key": apiKey,
   };
@@ -16,6 +26,18 @@ async function handleSubmit(event) {
     "https://api.humorapi.com/memes/random" + "?" + dataString,
   );
   const result = await response.json();
-  imgTag.src = result.url;
-  sessionStorage.setItem("apiKey", apiKey);
+  const message = result.message;
+  const isInvalid =
+    message ===
+    "You are not authorized. Please read https://humorapi.com/docs/#Authentication";
+
+  if (isInvalid) {
+    sessionStorage.removeItem("apiKey");
+    outputTag.innerText = "The API key you entered is invalid.";
+    imgTag.src = "";
+  } else {
+    outputTag.innerText = "";
+    imgTag.src = result.url;
+    sessionStorage.setItem("apiKey", apiKey);
+  }
 }
